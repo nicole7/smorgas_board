f3607866fd8960ac758d00d3e2e9d7b0475e2667
Karlie Romas <karlie.romas@gmail.com>
2017-11-24 13:13:05 -0500

    write rspec tests for user model that should validate uniqueness of username, validates a user has a username and validates a user's friendings association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is not valid without a username' do
+    user = User.new(username: nil)
+    expect(user).to_not be_valid
+  end
+
+  it { should validate_uniqueness_of(:username) }
+
+  it 'should have friends' do
+    user = User.reflect_on_association(:friendings)
+    expect(user.friends).to eq(:has_many)
+  end
+
 end
