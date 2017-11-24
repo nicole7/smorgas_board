ef8b6623ad5dd695e411a352d4ffaff4de90998d
Elise Poplawski <elise.poplawski@gmail.com>
2017-11-24 14:15:38 -0600

    add routing for adding friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
 
-  resources :users, only: [:create, :show]
+  resources :users, only: [:create, :show, :index]do
+    get '/friend_requests', to: 'friend_requests#index'
+    get '/friends', to: 'friendships#index'
+  end
   resources :sessions, only: [:create]
   resources :games
 
@@ -10,5 +13,13 @@ Rails.application.routes.draw do
   get 'register', to: 'users#new', as: 'register'
 
   root 'games#index'
+
+  # resources :users, only: [:show index] do
+  #   get '/friend_requests', to: 'friend_requests#index'
+  #   get '/friends', to: 'friendships#index'
+  # end
+
+  resources :requests, only: [:create, :update, :destroy]
+  resources :friendings, only: :destroy
 
 end
