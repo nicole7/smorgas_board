2165fd56858f47e9793a883468654a7726a31ace
Toby Ladislas <tladislas@gmail.com>
2017-11-24 12:19:28 -0600

    We should not git add all bleghh I'm Toby

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
+      flash[:notice] = "Registration successful!"
       if request.xhr?
         render :create
       else
