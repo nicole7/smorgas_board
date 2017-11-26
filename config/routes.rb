aed286522d2a5f2abd8b5d4d9fa2e3d4adb93d54
majesty86 <tladislas@gmail.com>
2017-11-26 00:14:36 -0600

    Try this?

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,21 @@
 Rails.application.routes.draw do
 
 
-  resources :users, only: [:create, :show, :index]do
+  resources :users, only: [:create, :show, :index] do
     get '/friend_requests', to: 'friend_requests#index'
     get '/friends', to: 'friendships#index'
   end
+
   resources :sessions, only: [:create]
   resources :games
 
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'register', to: 'users#new', as: 'register'
+  resources :requests, only: [:create, :update, :destroy]
+  resources :friendings, only: :destroy
 
   root 'games#index'
 
-  # resources :users, only: [:show index] do
-  #   get '/friend_requests', to: 'friend_requests#index'
-  #   get '/friends', to: 'friendships#index'
-  # end
-
-  resources :requests, only: [:create, :update, :destroy]
-  resources :friendings, only: :destroy
 
 end
